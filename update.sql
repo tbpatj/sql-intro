9633139ea14479e0323b25cf0414cee8d2eb58d6
Timothy Luebke <luebketj@gmail.com>
2021-11-08 15:25:31 -0700

    Finished the assignemnt

--- update.sql
+++ update.sql
@@ -4,11 +4,23 @@ SET fax = null
 WHERE fax = '';
 
 -- 2.
+UPDATE customer
+SET company = 'self'
+WHERE company = '';
+
 
 -- 3.
+UPDATE customer
+SET last_name = 'Thompson'
+WHERE first_name = 'Julia' AND last_name = 'Barnett';
 
 -- 4.
+UPDATE customer
+SET support_rep_id = 4
+WHERE email = 'luisroja@yahoo.cl';
 
 -- 5.
-
+UPDATE track
+SET composer = 'The darkness around us'
+WHERE composer = '' AND genre_id = 3;
 
